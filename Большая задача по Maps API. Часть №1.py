8ba72bfe21a4c88f9c4cdba225b113bfa2a61b83
Наташа <nat.dol@mail.ru>
2019-03-30 19:02:23 +0300

    -- Большая задача по Maps API. Часть №2

--- Большая задача по Maps API. Часть №1.py
+++ Большая задача по Maps API. Часть №1.py
@@ -10,16 +10,41 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('task1.ui', self)
         self.response = None
+        self.delta = 0
+        self.latt = 0
+        self.lonn = 0
+        self.spnn = 1
+        self.x = 180
         self.initUI()
 
     def initUI(self):
         self.setStyleSheet("QWidget { background-color: #ffd9b3}")
         self.sh.clicked.connect(self.showw)
+        self.up.clicked.connect(self.upp)
+        self.down.clicked.connect(self.downn)
+
+    def upp(self):
+        x = float(self.spn.text())
+        if x < self.x - self.delta:
+            self.spnn = float(self.spn.text())*1.5
+        self.spn.setText(str(self.spnn))
+
+    def downn(self):
+        x = float(self.spn.text())
+        if x > self.delta:
+            self.spnn = float(self.spn.text())/1.5
+        self.spn.setText(str(self.spnn))
 
     def showw(self):
-        coords, spn = self.coords.text(), self.spn.text()
-        map_request = "http://static-maps.yandex.ru/1.x/?ll={}&spn={}&l=map".format(coords, spn)
-        self.response = requests.get(map_request)
+        api_server = "http://static-maps.yandex.ru/1.x/"
+        self.latt, self.lonn, self.spnn = self.lat.text(), self.lon.text(), self.spn.text()
+
+        params = {
+            "ll": ",".join([self.lonn, self.latt]),
+            "spn": ",".join([self.spnn, self.spnn]),
+            "l": "map"
+        }
+        self.response = requests.get(api_server, params=params)
         pixm = self.writefile()
         self.pic.setPixmap(pixm.scaled(761, 541))
 
